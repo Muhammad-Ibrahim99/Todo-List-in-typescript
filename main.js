813e32f9afb75619388a5511aa1fbc38ddac41c5
Muhammad Ibrahim <160318731+Muhammad-Ibrahim99@users.noreply.github.com>
2024-04-26 17:43:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//TODO APP IN TYPESCRIPT:
+import inquirer from "inquirer";
+//user variable:
+let todos = [];
+while (true) {
+    let input = await inquirer.prompt([
+        {
+            name: "Todoitem",
+            type: "input",
+            message: 'add todo item in listing'
+        },
+        {
+            name: "addMore",
+            type: 'list',
+            choices: ["yes", "no"]
+        }
+    ]);
+    //output program:
+    const { Todoitem, addMore } = input;
+    todos.push(Todoitem);
+    if (addMore == "no") {
+        console.log("Todolist:");
+        //add more item:
+        for (let i = 0; i < todos.length; i++) {
+            console.log(todos[i]);
+        }
+        //exit loop:
+        break;
+    }
+}
